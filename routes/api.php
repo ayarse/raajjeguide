7b4c55e5611883266453f54e71b6d4244c6fa36d
Ayas Nasih <ayasnasih@gmail.com>
2020-06-17 15:36:02 +0500

    Creates favorites api url

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('favorites', function() {
+
+    $favorites = explode(",", request()->cookie('favorites'));
+    $favs = \App\Listing::select('id', 'title', 'phone')->get()->filter(function ($item) use ($favorites) {
+        return in_array($item->id, $favorites);
+    })->values();
+    return response()->json($favs);
+
+})->name('favorites');
