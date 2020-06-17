59c9c9fe9eec4c24abf7b588bf2bd3c54890557e
Ayas Nasih <ayasnasih@gmail.com>
2020-06-17 16:32:00 +0500

    Add google analytics

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -42,7 +42,7 @@
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="images/icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="images/icon-114x114.png">
-
+    @include('partials.ga')
   </head>
   <body>
 
